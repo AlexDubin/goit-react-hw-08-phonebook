d6af39f0d3d85210b3e5a784abaf0c56ab1e36f1
Oleksandr Dubin <102242242+AlexDubin@users.noreply.github.com>
2023-08-23 21:59:55 +0200

    first commit

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -1,3 +1,38 @@
+// import React from 'react';
+// import { logoutUser } from 'redux/operations';
+// import { useNavigate } from 'react-router-dom';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { Button } from '@mui/material';
+// import { selectUserUserData } from 'redux/selectors';
+
+// const UserMenu = () => {
+//   const navigate = useNavigate();
+//   const dispatch = useDispatch();
+//   const user = useSelector(selectUserUserData);
+
+//   const handleLogout = () => {
+//     dispatch(logoutUser());
+//     navigate('/');
+//   };
+
+//   return (
+//     <div>
+//       <h1>User Information:</h1>
+//       <p>
+//         <b>User Name:</b> {user.name}
+//       </p>
+//       <p>
+//         <b>User Email:</b> {user.email}
+//       </p>
+//       <Button variant="contained" sx={{ mt: 2 }} onClick={handleLogout}>
+//         Logout
+//       </Button>
+//     </div>
+//   );
+// };
+
+// export default UserMenu;
+
 import React from 'react';
 import { logoutUser } from 'redux/operations';
 import { useNavigate } from 'react-router-dom';
@@ -18,17 +53,24 @@ const UserMenu = () => {
   return (
     <div>
       <h1>User Information:</h1>
-      <p>
-        <b>User Name:</b> {user.name}
-      </p>
-      <p>
-        <b>User Email:</b> {user.email}
-      </p>
-      <Button variant="contained" sx={{ mt: 2 }} onClick={handleLogout}>
-        Logout
-      </Button>
+      {user ? (
+        <>
+          <p>
+            <b>User Name:</b> {user.name}
+          </p>
+          <p>
+            <b>User Email:</b> {user.email}
+          </p>
+          <Button variant="contained" sx={{ mt: 2 }} onClick={handleLogout}>
+            Logout
+          </Button>
+        </>
+      ) : (
+        <p>No user information available</p>
+      )}
     </div>
   );
 };
 
 export default UserMenu;
+
